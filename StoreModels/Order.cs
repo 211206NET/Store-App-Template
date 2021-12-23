f738f4b15a0f39005516a79678352d47621a1743
Juniper Song <songminseonk@gmail.com>
2021-12-23 12:54:08 -0500

    added reference, minor changes to models

--- StoreModels/Order.cs
+++ StoreModels/Order.cs
@@ -13,10 +13,13 @@ public class Order
         //a method that would go through each lineitem in LineItems property
         //and sets the total property of the particular order object
         decimal total = 0;
-        foreach(LineItem lineitem in this.LineItems)
+        if(this.LineItems?.Count > 0)
         {
-            //multiply the product's price by how many we're buying
-            total += lineitem.Item.Price * lineitem.Quantity;
+            foreach(LineItem lineitem in this.LineItems)
+            {
+                //multiply the product's price by how many we're buying
+                total += lineitem.Item.Price * lineitem.Quantity;
+            }
         }
         this.Total = total;
         return total;
